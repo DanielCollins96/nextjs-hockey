32a1b0984fe5d276623bd18d2d3c0b0d4ab3c2f3
Daniel Collins <danielmcollins1996@gmail.com>
2021-01-03 17:00:29 -0700

    added chart

--- pages/teams/[id].js
+++ pages/teams/[id].js
@@ -1,13 +1,38 @@
 import { useQuery, useQueries } from 'react-query';
-
+import { letterFrequency } from '@visx/mock-data';
+import { Group } from '@visx/group';
+import { Bar } from '@visx/shape';
+import { scaleLinear, scaleBand } from '@visx/scale';
 import s from './team.module.css';
 
 export default function TeamPage({id}) {
-    // const response = useQuery('fetchPlayer', async () => {
-    //     const res = await fetch(`https://statsapi.web.nhl.com/api/v1/teams/${id}`)
-    //     const teamRes = await res.json()
-    //     return teamRes.teams
-    // })
+    const testData = letterFrequency;
+    const width = 500;
+    const height = 500;
+    const margin = { top: 20, bottom: 20, left: 20, right: 20 };
+
+    const xMax = width - margin.left - margin.right;
+    const yMax = height - margin.top - margin.bottom;
+
+    const x = d => d.letter;
+    const y = d => +d.frequency * 100;
+
+    const xScale = scaleBand({
+        range: [0, xMax],
+        round: true,
+        domain: testData.map(x),
+        padding: 0.4,
+    });
+    const yScale = scaleLinear({
+        range: [yMax, 0],
+        round: true,
+        domain: [0, Math.max(...testData.map(y))],
+    });
+
+    const compose = (scale, accessor) => dataVal => scale(accessor(dataVal));
+    const xPoint = compose(xScale, x);
+    const yPoint = compose(yScale, y);
+    
     const result = 
     useQueries
     ([
@@ -27,7 +52,7 @@ export default function TeamPage({id}) {
     const { [0]: {data: season_data, isLoading: season_loading, isError: isSErr, error: sErr}, [1]: { data: team_data, isLoading: team_loading, isError: isTErr, error:  tErr} } = result
     // const { isLoading, isError, data, error } = response
     // console.log(id)
-    console.log(result)
+    console.log(season_data)
     // if (season_loading || team_loading) {
     //     return <span>Loading...</span>
     //   }
@@ -37,16 +62,31 @@ export default function TeamPage({id}) {
     // }
     return (
         <div>
-        {!isTErr && team_data[0] ? 
+        {!isTErr && !!team_data ? 
         <div>
         <h3>{team_data[0].name}</h3>
-        {/* <p>{JSON.stringify(team_data)}</p> */}
         <p>{team_data[0].firstYearOfPlay}</p>
         <p>{team_data[0].officialSiteUrl}</p>
         </div>
         :
         <p>Data Not Found</p>}
-
+        {/* <p>{JSON.stringify(team_data)}</p> */}
+        <svg width={width} height={height}>
+        {testData.map((d, i) => {
+            const barHeight = yMax - yPoint(d);
+            return (
+            <Group key={`bar-${i}`}>
+                <Bar
+                x={xPoint(d)}
+                y={yMax - barHeight}
+                height={barHeight}
+                width={xScale.bandwidth()}
+                fill="#fc2e1c"
+                />
+            </Group>
+            );
+        })}
+        </svg>
 
         </div>
     )
