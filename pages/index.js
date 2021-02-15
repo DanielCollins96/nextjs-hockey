75b8447600501c6aa30a1eb5bb9bed63dff2bc36
dc <danielmcollins1996@gmail.com>
2021-02-15 14:12:11 -0700

    puck on main screen

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,30 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import styles from '../styles/Home.module.css';
+// import Puck from '../public/ice-hockey-puck.svg';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <div className={styles.grid}>
-          Big dick
+          {/* <img src="/planet-3.svg" alt="Planet" /> */}
+          <motion.img 
+            src="/ice-hockey-puck.svg" 
+            alt="Puck" 
+            width="200" 
+            height="200"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: .1 }}
+            whileTap={{ scale: 0.9 }}
+            drag={true}
+          />
+        </div>
+        <div>
+          
         </div>
       </main>
 
