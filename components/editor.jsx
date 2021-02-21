e59ae3e21fdffaf1eadd7c7997490414db7b3d38
dc <danielmcollins1996@gmail.com>
2021-02-20 20:05:50 -0700

    got a text editor

--- components/editor.jsx
+++ components/editor.jsx
@@ -1,45 +1,34 @@
-import React, { useEffect, useMemo, useState, useCallback } from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import React, { useState, useEffect, useRef } from 'react'
+// import { CKEditor } from '@ckeditor/ckeditor5-react';
+// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-const TextEditor = () => {
-    const [value, setValue] = useState('')
+export default function TextEditor () {
+  const editorRef = useRef()
+  const [editorLoaded, setEditorLoaded] = useState(false)
+  const { CKEditor, ClassicEditor } = editorRef.current || {}
 
+  useEffect(() => {
+    editorRef.current = {
+      CKEditor: require('@ckeditor/ckeditor5-react').CKEditor,
+      ClassicEditor: require('@ckeditor/ckeditor5-build-classic')
+    }
+    setEditorLoaded(true)
+  }, [])
 
-    return (
-        <div>
-            <CKEditor
-              editor={ ClassicEditor }
-              data="<p>Hello from CKEditor 5!</p>"
-              onReady={ editor => {
-                  // You can store the "editor" and use when it is needed.
-                  console.log( 'Editor is ready to use!', editor );
-              } }
-              onChange={ ( event, editor ) => {
-                  const data = editor.getData();
-                  console.log( { event, editor, data } );
-              } }
-              onBlur={ ( event, editor ) => {
-                  console.log( 'Blur.', editor );
-              } }
-              onFocus={ ( event, editor ) => {
-                  console.log( 'Focus.', editor );
-              } }
-            />
-        </div>
-      )
-}
-
-const Toolbar = () => {
-  return (
-    <div>
-      {/* <button active={isBoldActive(editor)}>B</button> */}
-      <button>B</button>
-      {/* <button active={isItalicActive(editor)}>I</button> */}
-      <button>I</button>
-    </div>
+  return editorLoaded ? (
+    <CKEditor
+      editor={ClassicEditor}
+      data='<p>Hello from tha dawg pownd!</p>'
+      onInit={editor => {
+        // You can store the "editor" and use when it is needed.
+        console.log('Editor is ready to use!', editor)
+      }}
+      onChange={(event, editor) => {
+        const data = editor.getData()
+        console.log({ event, editor, data })
+      }}
+    />
+  ) : (
+    <div>Editor loading</div>
   )
 }
-
-export default TextEditor;
-
