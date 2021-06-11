f834b64cbb604849b1cad5123fc3133cc017de31
dc <danielmcollins1996@gmail.com>
2021-06-11 11:50:21 -0600

    pre tailwind

--- pages/index.js
+++ pages/index.js
@@ -10,40 +10,14 @@ const [games, setGames] = useState([]);
 useEffect(async() => {
   const data = await fetch('https://statsapi.web.nhl.com/api/v1/schedule');
   let gameSchedule = await data.json();
-  console.log(gameSchedule.dates[0].games) 
-  setGames(gameSchedule.dates[0].games);
+  console.log(gameSchedule.dates[0]?.games) 
+  setGames(gameSchedule.dates[0]?.games);
   // console.log(format(games[0].gameDate, 'YYYY-MM-DD'))
 },[])
 console.log(format(new Date("2021-03-23T23:00:00Z"), 'ppp'))
 // console.log(games[0].gameDate)
-const TickerText = () => {
-  // <Box bg="yellow" border="2px solid black" padding={2} w={256} display="inline-block">
-  return games?.map((game) => {
-    return (
-      <>
-      <Stack spacing={0} p={1} mx={.5}  border="black solid 1px">
-        <Text m={0} as="span" fontSize="xs"whiteSpace="nowrap">Home: {game.teams.home.team.name}: {game.teams.home.score}</Text>
-        <Text margin="0" as="span" fontSize="xs"whiteSpace="nowrap">Away: {game.teams.away.team.name}: {game.teams.home.score}</Text>
-        <Text m={0} as="span" fontSize="xs"whiteSpace="nowrap">{format(new Date(game.gameDate), 'ppp')}</Text>
-      </Stack>
-    </>
-    )
-  })
-  // return <p>hey</p>
-}
-
   return (
           <Box mt={1}>
-            <Box>
-          { games.length > 0 ?
-            <Ticker>
-              {() => <Flex color="red"><TickerText /></Flex>}
-              {/* {() => <TickerText />} */}
-            </Ticker>
-            :
-            null
-          }
-          </Box>
           <Flex justify="center">
           <motion.img 
             src="/ice-hockey-puck.svg" 
