93a5296db2568a0074d6edfc7d77f4ee33ebe8c1
dc <danielmcollins1996@gmail.com>
2021-06-12 11:59:13 -0600

    working react queries

--- pages/players/[id].js
+++ pages/players/[id].js
@@ -6,47 +6,40 @@ import { setNestedObjectValues } from 'formik';
 
 // https://statsapi.web.nhl.com/api/v1/people/8474056/stats/?stats=statsSingleSeason&season=20122013
 
+
 const PlayerPage = ({id}) => {
-    const [player, setPlayer] = useState(null)
-    const [playerStats, setPlayerStats] = useState(null)
-    const result = 
-    useQueries
-    ([
-        { queryKey: `fetch${id}Stats`, queryFn: async () => {
-            const res = await fetch(`https://statsapi.web.nhl.com/api/v1/people/${id}/stats?stats=yearByYear`);
-            const playerRes = await res.json()
-            console.log(`PLAYER RES`)
-            console.log(playerRes)
-            const playerStats = playerRes.stats[0].splits.map((szn) => {
-                return (
-                    {
-                        season: szn.season.substring(0,4)+'/'+szn.season.substring(4,8),
-                        team: szn.team.name,
-                        league: szn.league.name,
-                        gp: szn.stat.games,
-                        g: szn.stat.goals,
-                        a: szn.stat.assists,
-                        pts: szn.stat.points,
-                        pim: szn.stat.pim,
-                    }
-                )
-            })
-            console.log('fulll')
-            console.log(playerStats);
+    const fetchPlayer = async () => {
+        const res = await fetch(`https://statsapi.web.nhl.com/api/v1/people/${id}`);
+        const playerRes = await res.json()
+        return playerRes
+    }
+    
+    const fetchPlayerStats = async () => {
+        const res = await fetch(`https://statsapi.web.nhl.com/api/v1/people/${id}/stats?stats=yearByYear`);
+        const playerRes = await res.json()
+        const playerStats = playerRes.stats[0].splits.map((szn) => {
+            return (
+                {
+                    season: szn.season.substring(0,4)+'/'+szn.season.substring(4,8),
+                    team: szn.team.name,
+                    league: szn.league.name,
+                    gp: szn.stat.games,
+                    g: szn.stat.goals,
+                    a: szn.stat.assists,
+                    pts: szn.stat.points,
+                    pim: szn.stat.pim,
+                }
+            )
+        })
+        return playerStats
+    }
 
-            return playerStats
-            } 
-        },
-        { queryKey: 'fetchPlayer', queryFn: async () => {
-            const res = await fetch(`https://statsapi.web.nhl.com/api/v1/people/${id}`);
-            const playerRes = await res.json()
-            return playerRes
-            } 
-        },
-    ])
-    setPlayerStats(result[0].data)
-    setPlayer(result[1].data)
+    const { data: player, status: player_status } = useQuery(`player-${id}`, fetchPlayer);
+    const { data: playerStats, status: stats_status } = useQuery(`playerStats-${id}`, fetchPlayerStats);
 
+    console.log(playerStats)
+    console.log(player_status)
+    console.log(player)
     const columns = useMemo(
         () => [
              {
@@ -88,21 +81,21 @@ const PlayerPage = ({id}) => {
     )
     return (
         <div className="">
-            <p className="text-2xl font-bold">Player Stats</p>
-                {/* {player ? (
-                    <div className="">
-                     <h2>{player.people[0].fullName}</h2>
-                     <p>Birth Date: {player.people[0].birthDate}</p>
-                     <p>Nationality: {player.people[0].birthCountry}</p>
-                     <p>Primary Number: {player.people[0].primaryNumber}</p>
-                     <p>Age: {player.people[0].currentAge}</p>
-                    </div>
-                ) :
-                <p>Loading...</p>    
-            } */}
-            {/* {JSON.stringify(stat_data.stats[0].splits)} */}
-            {data ? <ReactTable columns={columns} data={data} /> : <p>Lookin</p>}
-            
+            <div className="flex flex-col items-center w-72 m-auto">
+                    {player_status === 'success' ? (
+                        <div className="">
+                        <p className="text-2xl font-bold">{player.people[0].fullName}</p>
+                        <p>Birth Date: {player.people[0].birthDate}</p>
+                        <p>Nationality: {player.people[0].birthCountry}</p>
+                        <p>Primary Number: {player.people[0].primaryNumber}</p>
+                        <p>Age: {player.people[0].currentAge}</p>
+                        </div>
+                    ) :
+                    <p>Loading...</p>    
+                }
+            </div>
+            {stats_status === 'success' ? <ReactTable columns={columns} data={playerStats} /> : <p>Loading...</p>}
+            {stats_status === 'error' ? <p>Error Fetching Stats.</p> : null}
         </div>
     )
 };
