c12a29ce25e864b51365170b1d4f9cfd8538f8fe
dc <danielmcollins1996@gmail.com>
2021-06-13 16:18:04 -0600

    no user button style

--- components/DropdownButton.js
+++ components/DropdownButton.js
@@ -17,7 +17,9 @@ const DropdownButton = () => {
     if(!user) {
         return (
             <div>
-            <button type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-6 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                <Link>
+                    <button type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-6 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                </Link>
             Login
             </button>
         </div>
