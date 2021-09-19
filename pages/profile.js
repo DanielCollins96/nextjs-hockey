82c8237c3ecfe8dfb1ba036f82a859adac6a9d5c
Dan <danielmcollins1996@gmail.com>
2021-09-19 15:30:56 -0600

    nuking amplify auth due to ''Shadown DOM''

--- pages/profile.js
+++ pages/profile.js
@@ -29,62 +29,8 @@ function Profile() {
 
   return (
     <div>
-      <div className="flex flex-col items-center sm:flex-row m-2 sm:items-start">
-        <div className="bg-white m-2 p-2">
-          { user && <h2 className="font-bold">Welcome, {user?.attributes?.email}</h2> }
-        </div>
-        <div className="w-3/4 flex flex-col p-2">
-          <label htmlFor="title" className="text-3xl mb-2">
-            <input type="text" onChange={changePost} className="p-1" placeholder="Enter title..." value={post?.title} name="title" id="title"/>
-          </label>
-          <SimpleMDE value={post?.content} onChange={value => setPost({...post, content: value})}/>
-          <button className="bg-blue-200 p-3 rounded inline-block" onClick={savePost} type="submit">Save Post</button>
-        </div>
-      </div>
-      {posts ? posts.map((post) => {
-        return <p>Hey</p> 
-      })
-      :
-      <p>whaaat</p>
-    }
-      <AmplifySignIn 
-        headerText="Sign In"
-        slot="sign-in"
-        usernameAlias="email"
-        formFields={[
-          {
-            type: "email",
-            label: "Email",
-            placeholder: "Please enter your Email",
-            required: true,
-          },
-          {
-            type: "password",
-            label: "Password",
-            placeholder: "Please enter your Password",
-            required: true,
-          },
-        ]}
-        />
-      <AmplifySignUp
-          slot="sign-up"
-          usernameAlias="email"
-          formFields={[
-            {
-              type: "email",
-              label: "Email",
-              placeholder: "Please enter your Email",
-              required: false,
-            },
-            {
-              type: "password",
-              label: "Password",
-              placeholder: "Please enter your Password",
-              required: true,
-            },
-          ]}
-          />
-        </div>
+      { user && <h2 className="font-bold">Welcome, {user?.attributes?.email}</h2> }
+    </div>
   )
 }
 
