62e1c69be23734a0da8ccdfa1c2a51c777dc25af
Dan <danielmcollins1996@gmail.com>
2021-09-19 23:26:41 -0600

    jesus amplify is a cluster fk

--- README.md
+++ README.md
@@ -28,6 +28,12 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 
 You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
 
+## Amplify Stuff
+
+### Commands
+
+- `amplify console api`
+
 ## Learn More
 
 To learn more about Next.js, take a look at the following resources:
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useAuth } from '../contexts/Auth';
 import { AmplifySignUp, AmplifySignIn, AmplifyAuthenticator,AmplifyAuthContainer,withAuthenticator } from '@aws-amplify/ui-react'
-import Amplify, { API } from 'aws-amplify';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import dynamic from 'next/dynamic';
 import "easymde/dist/easymde.min.css";
 import s from "./profile.module.css";
@@ -30,6 +30,18 @@ function Profile() {
   return (
     <div>
       { user && <h2 className="font-bold">Welcome, {user?.attributes?.email}</h2> }
+      <div className="flex flex-col items-center sm:flex-row m-2 sm:items-start">
+        <div className="bg-white m-2 p-2">
+          { user && <h2 className="font-bold">Welcome, {user?.attributes?.email}</h2> }
+        </div>
+        <div className="w-3/4 flex flex-col p-2">
+          <label htmlFor="title" className="text-3xl mb-2">
+            <input type="text" onChange={changePost} className="p-1" placeholder="Enter title..." value={post?.title} name="title" id="title"/>
+          </label>
+          <SimpleMDE value={post?.content} onChange={value => setPost({...post, content: value})}/>
+          <button className="bg-blue-200 p-3 rounded inline-block" onClick={savePost} type="submit">Save Post</button>
+        </div>
+      </div>
     </div>
   )
 }
