2557455b9cb9315def541e19170ad1fdadebb024
Dan <danielmcollins1996@gmail.com>
2021-10-26 22:22:07 -0600

    removing chakra imports

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import NextDocument, {Html, Head, Main, NextScript} from 'next/document';
-import { extendTheme, ColorModeScript } from "@chakra-ui/react"
 
-const config = {
-  initialColorMode: "light",
-  useSystemColorMode: false,
-}
-
-const theme = extendTheme({ config })
 
 class Document extends NextDocument {
     static async getInitialProps(ctx) {
@@ -22,7 +15,6 @@ class Document extends NextDocument {
                     <link rel="icon" sizes="96x96" href="/favicons/favicon.ico" />
                 </Head>
                 <body className="bg-gray-100">
-                    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
                     <Main />
                     <NextScript />
                 </body>
