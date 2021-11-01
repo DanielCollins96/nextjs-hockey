76bb7c8a5302d6952579f975d398421cadac26e4
Dan <danielmcollins1996@gmail.com>
2021-11-01 00:08:12 -0600

    amplify Pull

--- aws-exports.js
+++ aws-exports.js
@@ -8,6 +8,25 @@ const awsmobile = {
     "aws_user_pools_id": "us-east-1_LY4LFScSa",
     "aws_user_pools_web_client_id": "5bhs02o144c5dcv9l6p5b1jqeu",
     "oauth": {},
+    "aws_cognito_login_mechanisms": [
+        "EMAIL"
+    ],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 6,
+        "passwordPolicyCharacters": [
+            "REQUIRES_LOWERCASE"
+        ]
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ],
     "aws_appsync_graphqlEndpoint": "https://bnudyl5eybcajlmcfrhctcfrsi.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
