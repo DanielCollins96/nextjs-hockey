73087541e3f26fa5444665303f9627748fc62e9d
Dan <danielmcollins1996@gmail.com>
2022-08-28 23:18:02 -0600

    Adding adsense

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NextDocument, {Html, Head, Main, NextScript} from 'next/document';
-
+import Script from 'next/script'
 
 class Document extends NextDocument {
     static async getInitialProps(ctx) {
@@ -13,6 +13,8 @@ class Document extends NextDocument {
             <Html lang="en">
                 <Head>
                     <link rel="icon" sizes="96x96" href="/favicons/favicon.ico" />
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2056923001767627"
+     crossOrigin="anonymous"></script>
                 </Head>
                 <body className="bg-gray-100">
                     <Main />
